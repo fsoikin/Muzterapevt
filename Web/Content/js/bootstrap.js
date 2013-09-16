bef665d18c0e9f95f4cce2a1313ddfca3a18b9ca
Fyodor Soikin <name.fa@gmail.com>
2013-09-15 22:54:19 -0400

    Deleted remaining js files

--- Web/Content/js/bootstrap.js
+++
@@ -1,7 +0,0 @@
-﻿define(["require", "exports", "jQuery", "./lib/autobind"], function(require, exports, __$__, __ab__) {
-    
-    var $ = __$__;
-    var ab = __ab__;
-
-    ab.autobindAll($(".autobind"));
-});
