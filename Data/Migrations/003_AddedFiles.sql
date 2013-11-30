daf8bcd0bb2e4fd7d89ddbb29d4d2ccd43600fda
fsoikin <name.fa@gmail.com>
2013-11-29 22:37:26 -0500

    Added the Files migration script

---
+++ Data/Migrations/003_AddedFiles.sql
@@ -0,0 +1,23 @@
+﻿if not exists( select * from sys.tables where object_id = object_id('Files') ) 
+	create table Files (
+		Id int identity primary key,
+		Domain nvarchar(500),
+		FilePath nvarchar(500),
+		CreatedOn datetime2 not null,
+		OriginalFileName nvarchar(500),
+
+		Data varbinary(max),
+		ContentType nvarchar(50)
+	)
+GO
+
+if not exists( select * from sys.tables where object_id = object_id('FileVersions') ) 
+	create table FileVersions (
+		Id int identity primary key,
+		[File_Id] int references Files not null,
+		[Key] nvarchar(500),
+		CreatedOn datetime2 not null,
+		Data varbinary(max),
+		ContentType nvarchar(50)
+	)
+GO
