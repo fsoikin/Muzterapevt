6f3ac6a1b08a92221fe13e98d947ea502e319e29
Fyodor Soikin <name.fa@gmail.com>
2014-02-07 17:10:03 -0500

    Fixing the build.

--- Web/__Themes/_/tsd/serverTypes.d.ts
+++ Web/__Themes/_/tsd/serverTypes.d.ts
@@ -1,15 +1,15 @@
-/// <reference path="../../../UI/JS/Specialists/SpecialistRegistrationRequest.cs.d.ts" />
-/// <reference path="../../../UI/JS/Specialists/Organization.cs.d.ts" />
-/// <reference path="../../../UI/JS/Specialists/Country.cs.d.ts" />
-/// <reference path="../../../UI/JS/Specialists/SpecialistProfession.cs.d.ts" />
-/// <reference path="../../../UI/JS/Specialists/SpecialistSpecialization.cs.d.ts" />
-/// <reference path="../../../UI/JS/Specialists/SpecialistView.cs.d.ts" />
+/// <reference path="../../../../UI/JS/Specialists/SpecialistRegistrationRequest.cs.d.ts" />
+/// <reference path="../../../../UI/JS/Specialists/Organization.cs.d.ts" />
+/// <reference path="../../../../UI/JS/Specialists/Country.cs.d.ts" />
+/// <reference path="../../../../UI/JS/Specialists/SpecialistProfession.cs.d.ts" />
+/// <reference path="../../../../UI/JS/Specialists/SpecialistSpecialization.cs.d.ts" />
+/// <reference path="../../../../UI/JS/Specialists/SpecialistView.cs.d.ts" />
 
-/// <reference path="../../../UI/JS/Feedback/FeedbackQuestion.cs.d.ts" />
+/// <reference path="../../../../UI/JS/Feedback/FeedbackQuestion.cs.d.ts" />
 
-/// <reference path="../../../UI/JS/Text/TextView.cs.d.ts" />
+/// <reference path="../../../../UI/JS/Text/TextView.cs.d.ts" />
 
-/// <reference path="../../Models/Login/LoginMethodModel.cs.d.ts" />
-/// <reference path="../../Models/Login/LoginResultModel.cs.d.ts" />
+/// <reference path="../../../Models/Login/LoginMethodModel.cs.d.ts" />
+/// <reference path="../../../Models/Login/LoginResultModel.cs.d.ts" />
 
-/// <reference path="../../../UI/JS/Page/PageReorderRequest.cs.d.ts" />
+/// <reference path="../../../../UI/JS/Page/PageReorderRequest.cs.d.ts" />
