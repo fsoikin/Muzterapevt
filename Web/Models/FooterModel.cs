fdadf7bf996cb251524bd8c415b5c82f12f917a1
Fyodor Soikin <name.fa@gmail.com>
2014-02-10 20:08:13 -0500

    Designing Musicarium theme.

---
+++ Web/Models/FooterModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Mut.Models
+{
+	public class FooterModel
+	{
+		public string SiteName { get; set; }
+		public LayoutModel Layout { get; set; }
+	}
+}
