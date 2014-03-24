c24b9c16305add354baf2c31ab14282f4bd3c56e
Fyodor Soikin <name.fa@gmail.com>
2014-03-24 10:54:14 -0400

    Added colorful "Musicarium" markdown option

--- Plugins/Musicarium/Markdown.cs
+++ Plugins/Musicarium/Markdown.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Linq;
-using System.Net.Mail;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using erecruit.Composition;
-using erecruit.Utils;
-using Mut.Data;
 using Mut.UI;
 
 namespace Mut.Musicarium
 {
-	[Export]
 	class Markdown
 	{
-//		static object a = MarkdownCustomModule.Create( "" )
+		[Export]
+		static IMarkdownProvider MusicariumColorful = MarkdownProvider.Create( new MarkdownNodeDefinition<MarkdownParseArgs>(
+			"(?i)!(?'word'Musicarium|Музыкариум)", (_1,rgx,_2,_3) => new TextNode( 
+				"<span class='musicarium-text-logo'>" + string.Join( "", 
+					rgx.Groups["word"].Value.Select( (chr,idx) => string.Format( "<span class='l{1}'>{0}</span>", chr, idx ) ) )
+					+ "</span>",
+					encode: false ) ) );
 	}
 }
