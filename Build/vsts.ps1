893e69eaa21c3c865435661899c9293bd03a5a09
Fyodor Soikin <name.fa@gmail.com>
2015-12-15 12:23:14 -0500

    Tweaking build for VSTS.

--- Build/vsts.ps1
+++ Build/vsts.ps1
@@ -3,4 +3,5 @@ param( $Target )
 $root = [System.IO.Path]::GetFullPath( [System.IO.Path]::Combine( (Split-Path $MyInvocation.MyCommand.Path), ".." ) )
 cd $root
 Write-Host $root
+ls
 & build.ps1 $Target -Bootstrap
